a6910fe74b41dfdba6eccccd363d35748c9047fe
Sam Wlody <swlody@gmail.com>
2025-01-25 16:40:52 -0500

    Restructure CD, add supabase deployments (#1)
    * Update workflows and code

--- temp/20250124001630_create_cities_table.sql
+++
@@ -1,7 +0,0 @@
-create table
-cities (
-    id uuid not null primary key,
-    name text not null,
-    created_at timestamp not null default current_timestamp,
-    updated_at timestamp not null default current_timestamp
-)
